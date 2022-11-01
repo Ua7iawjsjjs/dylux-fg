d2d85ef29c6a2e32cac688e308bc6d704a10e825
Ua7iawjsjjs <117027176+Ua7iawjsjjs@users.noreply.github.com>
2022-11-01 19:51:20 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,13 +3,13 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['59172945992', 'FG98', true],
-  ['5493885839638'], 
-  ['5492622689586'] 
+  ['212674874107', 'FG98', true],
+  ['212674874107'], 
+  ['212674874107'] 
 ] //Numeros de owner 
 
-global.mods = ['573125484672'] 
-global.prems = ['50489079501', '5219631769130', '573125484672']
+global.mods = ['212674874107'] 
+global.prems = ['212674874107', '212674874107', '212674874107']
 global.APIs = { // API Prefix
   // name: 'https://website'
   xteam: 'https://api.xteam.xyz', 
@@ -26,16 +26,16 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'DyLux┃ᴮᴼᵀ' 
-global.author = '@fg98._' 
-global.igfg = '▢ Sígueme en Instagram\nhttps://www.instagram.com/fg98._\n' 
-global.dygp = 'https://chat.whatsapp.com/IO9jmpI72ejHiN4unRZleU'
-global.fgsc = 'https://github.com/FG98F/dylux-fg' 
-global.fgyt = 'https://youtube.com/fg98f'
-global.fgpyp = 'https://paypal.me/fg98f'
-global.fglog = 'https://i.imgur.com/Owmb93c.png' 
+global.packname = 'ZENX1 🎃' 
+global.author = 'yassen 🎃' 
+global.igfg = '▢ Sígueme en Instagram\nhttps://www.instagram.com/7x8.q_' 
+global.dygp = 'https://chat.whatsapp.com/DNZaLXU8nAW3jfqdxmsU3z'
+global.fgsc = '.' 
+global.fgyt = '.'
+global.fgpyp = '.'
+global.fglog = 'https://telegra.ph/file/5a48faed66eee1d8e05e8.jpg' 
 
-global.wait = '*⌛ _Cargando..._*\n*▰▰▰▱▱▱▱▱*'
+global.wait = '*⌛ _loading..._*\n*▰▰▰▱▱▱▱▱*'
 global.rwait = '⌛'
 global.dmoji = '🤭'
 global.done = '✅'
